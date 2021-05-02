9a10a5ab528a027d0cb57dab5a84d8fbe17873f3
Yedenbai Madi <makosh24@mail.ru>
2021-05-02 19:17:24 +0600

    start

---
+++ admin/js/admin.js
@@ -0,0 +1,30 @@
+function init() {
+    $.post(
+        "core.php",
+        {
+            "action" : "init"
+        },
+        showGoods
+    );
+}
+
+function showGoods(data) {
+    data = JSON.parse(data);
+    console.log(data);
+    var out='<select>';
+    out +='<option data-id="0">Новый товар</option>';
+    for (var id in data) {
+        out +=`<option data-id="${id}">${data[id].name}</option>`;
+    }
+    out +='</select>';
+    $('.goods-out').html(out);
+    $('.goods-out select').on('change', selectGoods);
+}
+
+function selectGoods() {
+    console.log(yes);
+}
+
+$(document).ready(function () {
+   init();
+});
